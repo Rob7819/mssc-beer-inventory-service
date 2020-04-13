fd446ca8bf10cd4525cdf4d13857edb29a595246
Rob <rjboyce@teksavvy.com>
2020-04-13 16:51:48 -0400

    Implemented bootstrap profile for Spring Cloud

--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-04-03T19:14:22-0400",
+    date = "2020-04-13T16:04:45-0400",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.3 (Oracle Corporation)"
 )
 @Component
