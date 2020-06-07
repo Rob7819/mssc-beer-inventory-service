d3a5fcc90b4e81225229837ae11eda57d9fc9047
Rob <rjboyce@teksavvy.com>
2020-06-07 18:09:52 -0400

    Updated with Docker layers build

--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-04-13T19:16:01-0400",
+    date = "2020-06-07T18:03:29-0400",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.3 (Oracle Corporation)"
 )
 @Component
